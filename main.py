a9d2fd6ae5f53e6d0c6c052be0ea05f93fc13277
legnain <rlegnain@ymail.com>
2015-05-28 03:23:31 -0400

    test

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ Created on 2015-05-27
 import csv
 import os
 import sys
-from xlsxwriter.workbook import Workbook
+import xlsxwriter
 
 
 
 if __name__ == '__main__':
     
-    #fileName = "emails.csv"   
-    fileName = raw_input (" Enter the path and file name (e.g., path/filename.csv): ")
+    fileName = "test"
+    #fileName = raw_input (" Enter the file name (e.g., path/filename.csv): ")
     
-    if not os.path.isfile(fileName):          # check if the file is exist
-        print " the file is not "
-        sys.exit()                            # terminate the program
+    #if not os.path.isfile(fileName):          # check if the file is exist
+    #    print " the file or directory is not found"
+    #    sys.exit()                            # terminate the program
         
-    workbook  = Workbook(fileName + '.xlsx')
-    worksheet = workbook.add_worksheet()    
+    excelFile  = xlsxwriter.Workbook(fileName + '.xlsx')
+    worksheet = excelFile.add_worksheet()    
     
-    with open(fileName, 'rb') as f:
+    with open(fileName + ".csv", 'rb') as f:
         content = csv.reader(f)
         for index_row, data_in_row in enumerate(content):
             for index_col, data_in_cell in enumerate(data_in_row):
                 worksheet.write(index_row, index_col, data_in_cell)
     
-    workbook.close()
-    print " works"
+    excelFile.close()
+    print " === Conversion is done ==="
